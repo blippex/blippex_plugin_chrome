7fb065e4e79bdd589915d3225847fbe296b6df23
Dmitry Dmitriev <dd@archify.com>
2013-06-25 01:33:27 +0300

    Updated popup layout, added skipping of blippex pages ARCHIFY-1547

--- plugin/common/js/browser/tabs.js
+++ plugin/common/js/browser/tabs.js
@@ -2,7 +2,7 @@ blippex.define('blippex.browser.tabs', {
   check: function(oArgs){
 		var url = (oArgs.url || '');
 		var status = blippex.config.status.ok;
-		if (url.indexOf("127.0.0.1") > -1) {
+		if (url.indexOf("127.0.0.1") > -1 || url.indexOf("blippex.org") > -1) {
 			status = blippex.config.status.skip;
 		} else if (url.indexOf("https:") == 0) {
 			if (blippex.browser.settings.get('nohttps')) {
